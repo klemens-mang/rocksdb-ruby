40181603498e5e836b9333827c743be1e41e0ec0
Klemens Mang <klemens.mang@me.com>
2016-12-15 11:40:28 +0100

    Removed have_library and have_header check

--- ext/rocksdb/extconf.rb
+++ ext/rocksdb/extconf.rb
@@ -3,9 +3,7 @@ require "mkmf"
 dir_config('rocksdb')
 RbConfig::CONFIG["CPP"] = "g++ -E -std=gnu++11"
 
-if have_header('rocksdb/db.h') and have_library('rocksdb') 
-  $CPPFLAGS << " -std=gnu++11"
-  create_makefile("RocksDB/RocksDB")
-else
-  abort "can't find header or library of rocksdb"
-end
+have_header('rocksdb/db.h') 
+have_library('rocksdb') 
+$CPPFLAGS << " -std=gnu++11"
+create_makefile("RocksDB/RocksDB")
